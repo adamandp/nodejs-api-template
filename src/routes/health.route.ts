7cafde819fe2b31f3225f29207b8fece31e7cec9
adamandp <adamanandaputra@gmail.com>
2025-01-21 02:57:32 +0700

    feat: fix husky

--- src/routes/health.route.ts
+++ src/routes/health.route.ts
@@ -1,11 +1,11 @@
-import { Request, Response, Router } from "express";
-import { logger } from "../utils/logger";
+import { Request, Response, Router } from 'express'
+import { logger } from '../utils/logger'
 
-const HealthRouter: Router = Router();
+const HealthRouter: Router = Router()
 
-HealthRouter.get("/", (req: Request, res: Response) => {
-  logger.info("success get health");
-        res.status(200).send({ status: "200", data: "hello world" });
-});
+HealthRouter.get('/', (req: Request, res: Response) => {
+  logger.info('success get health')
+      res.status(200).send({ status: '200', data: 'hello world' })
+})
 
-export { HealthRouter };
+export { HealthRouter }
